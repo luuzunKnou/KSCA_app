3fe849741bc4e5a1b4472f4d4f305ba91b9efb76
luuzunKnou <luuzun@knou.ac.kr>
2019-03-27 08:18:45 +0900

    Design Working...

--- app/src/main/java/com/luuzun/ksca/MenuActivity.java
+++ app/src/main/java/com/luuzun/ksca/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.luuzun.ksca;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -7,6 +8,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -52,7 +54,6 @@ public class MenuActivity extends AppCompatActivity
 
         View nav_header_view = navigationView.getHeaderView(0);
 
-        /*  */
         Intent intent = getIntent();
         String id = intent.getStringExtra("id");
         Permission permission = (Permission) intent.getSerializableExtra("permission");
@@ -110,7 +111,27 @@ public class MenuActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_log_out) {
+            AlertDialog.Builder alert_confirm = new AlertDialog.Builder(this);
+            alert_confirm.setMessage("로그아웃 하시겠습니까?").setCancelable(false).setPositiveButton("확인",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(MenuActivity.this, LoginActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            startActivity(intent);
+                        }
+                    }).setNegativeButton("취소",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            return;
+                        }
+                    });
+            AlertDialog alert = alert_confirm.create();
+            alert.show();
+
             return true;
         }
 
